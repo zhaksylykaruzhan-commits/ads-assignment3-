1968cb81284ae71148fd674573da1823d6d6e94e
Zhaksylyk Aruzhan -2511! <zhaksylyk.aruzhan.alash@gmail.com>
2026-05-09 16:06:01 +0500

    Update and rename b2.cpp to b2. binary..cpp

---
+++ b2. binary..cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int binarySearch(int arr[], int size, int target)
+{
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {14, 51, 25, 55, 27, 80, 56, 40, 37, 93};
+    int size = 10;
+
+    int target = 93;
+
+    bubbleSort(arr, size);
+
+    int result = binarySearch(arr, size, target);
+
+    cout << result << endl;
+
+    return 0;
+}
